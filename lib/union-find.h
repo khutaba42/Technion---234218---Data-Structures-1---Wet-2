e0cb6b836d98ee73004c59bf032588e8f8f6cf6d
khutaba42 <khutaba.42@gmail.com>
2023-06-14 07:25:33 +0300

    gpt suggests this solution, needs testing for union-find

--- lib/union-find.h
+++ lib/union-find.h
@@ -10,42 +10,48 @@
  * the first element in the array (element at index 0) is not used for the sake of simplicity
  * meaning the array is of size N+1
  * all function return 0 instead of an index value in case of out of out-of-range or no actual result to return
-*/
+ */
 // {1,...,N}
 class UnionFind
 {
-    //typedef int /*_group*/ int;
-    //typedef int /*_element*/ int;
+    // typedef int /*_set*/ int;
+    // typedef int /*_element*/ int;
 public:
     UnionFind(int N = 0);
     ~UnionFind();
 
-    // 
+    //
     /**
      * @brief creates a set of the integer i like we learned
      * @return 0 in case of out-of-range integer or element is in a set,
      * otherwise returns the integer (set/group)
-    */
-    /*_group*/ int make_set(/*_element*/ int i);
-    // 
+     */
+    /*_set*/ int make_set(/*_element*/ int i);
+    //
     /**
      * @brief finds the root of integer i like we learned
      * @return 0 in case of out-of-range integer or element is NOT in a set,
      * otherwise returns the root/set-representative
-    */
-    /*_group*/ int find(/*_element*/ int i);
+     */
+    /*_set*/ int find(/*_element*/ int i);
     /**
      * @brief like we learned
      * @return 0 in case of out-of-range integer or one of the parameters is NOT a root/set-representative,
      *  otherwise returns the new root/set-representative of the newly created set (the bigger tree's root)
-    */
-    /*_group*/ int unionize(/*_group*/ int smaller, /*_group*/ int bigger);
+     */
+    /*_set*/ int unionize(/*_set*/ int set_A, /*_set*/ int set_B);
+
+    /**
+     * @brief takes in a pointer to an array of heights and update the heights of the boxes
+     */
+    void add_heights(int *heights_arr, int len);
 
 private:
     int __size_of_array;
     int *__sizes_array;
     /*_element*/ int *__parents_array;
-
+    int *__rel_heights_of_boxes; // the rel height of a root is always it's true height
+    int *__real_height_of_towers;
     //  helper
 
     inline int get_size() const
@@ -79,6 +85,22 @@ private:
         {
             i = __parents_array[i];
         }
+        return i;
+    }
+
+    /*_element*/ int get_root_and_height_sum(/*_element*/ int i, int &sumR) const
+    {
+        if (!element_in_set(i))
+        {
+            return 0;
+        }
+        sumR = 0;
+        while (__parents_array[i] /*exists*/)
+        {
+            sumR += __rel_heights_of_boxes[i];
+            i = __parents_array[i];
+        }
+        // sum now has the sum of the heights up to the root (not included because of the __parents_array[i] != in the while)
         return i;
     }
 
@@ -143,7 +165,9 @@ public:
 
 UnionFind::UnionFind(int N) : __size_of_array(N),
                               __sizes_array(nullptr),
-                              __parents_array(nullptr)
+                              __parents_array(nullptr),
+                              __rel_heights_of_boxes(nullptr),
+                              __real_height_of_towers(nullptr)
 {
     if (N < 0)
     {
@@ -151,6 +175,8 @@ UnionFind::UnionFind(int N) : __size_of_array(N),
     }
     __sizes_array = new int[N + 1]();
     __parents_array = new int[N + 1]();
+    __rel_heights_of_boxes = new int[N + 1]();
+    __real_height_of_towers = new int[N + 1]();
     for (int i = 1; i <= N; i++)
     {
         make_set(i);
@@ -161,9 +187,11 @@ UnionFind::~UnionFind()
 {
     delete[] __sizes_array;
     delete[] __parents_array;
+    delete[] __rel_heights_of_boxes;
+    delete[] __real_height_of_towers;
 }
 
-/*_group*/ int UnionFind::make_set(/*_element*/ int i)
+/*_set*/ int UnionFind::make_set(/*_element*/ int i)
 {
     if (!element_in_range(i) || __sizes_array[i])
     {
@@ -173,33 +201,68 @@ UnionFind::~UnionFind()
     return i;
 }
 
-/*_group*/ int UnionFind::find(/*_element*/ int i)
+/*_set*/ int UnionFind::find(/*_element*/ int i)
 {
-    /*_element*/ int root = get_root(i);
-
-    // update parents
-    while (__parents_array[i] /*exists*/)
-    {
-        __parents_array[i] = root;
-        i = __parents_array[i];
-    }
-
-    return root;
-}
-
-/*_group*/ int UnionFind::unionize(/*_group*/ int smaller, /*_group*/ int bigger)
-{
-    if (!element_in_set(smaller) || !element_in_set(bigger) || __parents_array[smaller] || __parents_array[bigger])
+    if (!element_in_set(i))
     {
         return 0;
     }
-    // here we are sure that smaller and bigger are groups
-    if (__sizes_array[bigger] < __sizes_array[smaller]) 
-        std::swap(bigger, smaller); // swap the smaller and the bigger so they fit their names (note that this DOESN'T affect the arrays)
-    __parents_array[smaller] = bigger;
-    __sizes_array[bigger] += __sizes_array[smaller];
-    __sizes_array[smaller] = 0;
-    return bigger;
+    int old_parent = i;
+    /*_element*/ int root = __parents_array[i];
+    while (root != __parents_array[root])
+    {
+        // store the parent for path compression later
+        int next_old_parent = __parents_array[old_parent];
+        // path compression
+        __parents_array[old_parent] = __parents_array[root];
+        if (old_parent != root) // don't update height if you are at the root
+        {
+            // update the relative height
+            __rel_heights_of_boxes[old_parent] += __rel_heights_of_boxes[root];
+        }
+        // move up the tree
+        old_parent = next_old_parent;
+        root = __parents_array[old_parent];
+    }
+    return root;
+}
+
+/*_set*/ int UnionFind::unionize(/*_set*/ int set_A, /*_set*/ int set_B)
+{
+    if (!element_in_set(set_A) || !element_in_set(set_B) || __parents_array[set_A] || __parents_array[set_B])
+    {
+        return 0;
+    }
+    // make set_B the root of set_A if set_B has greater or equal height
+    if (__real_height_of_towers[set_B] >= __real_height_of_towers[set_A])
+    {
+        // update parent
+        __parents_array[set_A] = set_B;
+        __sizes_array[set_B] += __sizes_array[set_A];
+        // update the height of set_B (if needed)
+        __real_height_of_towers[set_B] = std::max(__real_height_of_towers[set_B], __rel_heights_of_boxes[set_A] + __real_height_of_towers[set_A]);
+    }
+    else
+    {
+        // update parent
+        __parents_array[set_B] = set_A;
+        __sizes_array[set_A] += __sizes_array[set_B];
+        // update the height of set_A
+        __real_height_of_towers[set_A] = std::max(__real_height_of_towers[set_A], __rel_heights_of_boxes[set_B] + __real_height_of_towers[set_B]);
+    }
+    return __parents_array[set_A];
+}
+
+void UnionFind::add_heights(int *heights_arr, int len)
+{
+    if (len != get_size())
+    {
+        return;
+    }
+    for (int i = 0; i < len; i++)
+    {
+        __rel_heights_of_boxes[i + 1] = heights_arr[i];
+    }
 }
 
 #endif // _UNION_FIND_H_
